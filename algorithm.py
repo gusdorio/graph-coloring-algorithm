03eed371f89cefc6db719c9c1816bb24281628db
AdvancedSeller <104785073+AdvancedSeller@users.noreply.github.com>
2024-04-05 10:14:11 -0300

    Instantiating the algorithm

--- algorithm.py
+++ algorithm.py
@@ -1,16 +1,27 @@
-# Function to get the matrix of the adjacency for each node
-def adjacency(graph):
-  dic = {}
-  for n in graph.nodes():
-    neighbors = list(graph.neighbors(n))
-    dic[n] = neighbors
-  return dic
+# Function to get the matrix of adjacency for each node
+import networkx as nx
 
 
-# Function to define clicks
-# def clicks(dic):
-#   count = 0
-#   for i in dic[count]:
-#     for n in range(dic[count]):
+class Clicks:
+    adjacency = {}
+    clicks = {}
+
+    def __init__(self, graph: 'nx.Graph'):
+        self.graph = graph  # is expected a nx object...
+        self.adjacency_add(self.graph)
+
+    def adjacency_add(self, graph):
+        # Add the adjacency of each node
+        for n in graph.nodes():
+            neighbors = list(graph.neighbors(n))
+            self.adjacency[n] = neighbors
+
+    # Function to define clicks
+    # def clicks(dic):
+    #   count = 0
+    #   for i in dic[count]:
+    #     for n in range(dic[count]):
 
 
+# x = Clicks(nx.gnp_random_graph(10, .3))
+# print(x.adjacency)
