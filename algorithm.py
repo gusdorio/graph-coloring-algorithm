d3acae6705d5293a0b9cdfb155e0abbf0a57554f
AdvancedSeller <104785073+AdvancedSeller@users.noreply.github.com>
2024-04-08 18:28:29 -0300

    defining pair of click into the graph

--- algorithm.py
+++ algorithm.py
@@ -4,11 +4,12 @@ import networkx as nx
 
 class Clicks:
     adjacency = {}
-    clicks = {}
+    clicks = []
 
     def __init__(self, graph: 'nx.Graph'):
         self.graph = graph  # is expected a nx object...
         self.adjacency_add(self.graph)
+        self.clicks_search(self.graph)
 
     def adjacency_add(self, graph):
         # Add the adjacency of each node
@@ -16,12 +17,18 @@ class Clicks:
             neighbors = list(graph.neighbors(n))
             self.adjacency[n] = neighbors
 
-    # Function to define clicks
-    # def clicks(dic):
-    #   count = 0
-    #   for i in dic[count]:
-    #     for n in range(dic[count]):
+    def clicks_search(self, adjacency):
+        # Function to define clicks
+        node = 0
+        while node < 10:  # STEP 1 -> DEFINING PAIRS
+            click = []
+            for item in adjacency[node]:
+                if [node, item] not in self.clicks:
+                    if [item, node] not in self.clicks:
+                        self.clicks.append([node, item])
+            node += 1
+    # STEP 2 -> DEFINING SETS (?)
 
 
-# x = Clicks(nx.gnp_random_graph(10, .3))
-# print(x.adjacency)
+x = Clicks(nx.gnp_random_graph(10, .3))
+print(x.clicks)
