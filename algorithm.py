3b4cf11bfc885af647419c8e4ca0b01fad6bc290
AdvancedSeller <104785073+AdvancedSeller@users.noreply.github.com>
2024-04-09 14:40:37 -0300

    finalization of the clicks search algorithm

--- algorithm.py
+++ algorithm.py
@@ -9,7 +9,8 @@ class Clicks:
     def __init__(self, graph: 'nx.Graph'):
         self.graph = graph  # is expected a nx object...
         self.adjacency_add(self.graph)
-        self.clicks_search(self.graph)
+        self.size_2(self.graph)
+        self.size_n(self.graph)
 
     def adjacency_add(self, graph):
         # Add the adjacency of each node
@@ -17,8 +18,8 @@ class Clicks:
             neighbors = list(graph.neighbors(n))
             self.adjacency[n] = neighbors
 
-    def clicks_search(self, adjacency):
-        # Function to define clicks
+    def size_2(self, adjacency):
+        # Function to define clicks of size two
         node = 0
         while node < 10:  # STEP 1 -> DEFINING PAIRS
             click = []
@@ -27,8 +28,29 @@ class Clicks:
                     if [item, node] not in self.clicks:
                         self.clicks.append([node, item])
             node += 1
-    # STEP 2 -> DEFINING SETS (?)
+
+    def size_n(self, adjacency):
+        # Define clicks of size > 2
+        node = 0
+        while node < 10:
+            actual = adjacency[node]
+            click = []
+            if len(actual) > 1:
+                for i in actual:
+                    another = adjacency[i]
+                    for n in another:
+                        if i != n:
+                            if n in actual and n not in click:
+                                click.append(n)
+                                if node not in click: click.append(node)
+                if len(click) > 2 and click not in self.clicks:  # else, actually are inside the clicks...
+                    self.clicks.append(click)
+            node += 1
 
 
-x = Clicks(nx.gnp_random_graph(10, .3))
-print(x.clicks)
+"""
+For test purposes, you can see the return of a instance of Clicks class
+"""
+# x = Clicks(nx.gnp_random_graph(10, .3))
+# print(x.adjacency)
+# print(x.clicks)
